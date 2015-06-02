1f23a05d42eb8b2a326e64ba507559cda533674e
idmahardika <id.mahardika@gmail.com>
2015-06-03 00:48:45 +0700

    New Start
    Just Added a few things

---
+++ libs/dbconn.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "SEO_commerce"; //tinggal ganti nama db
+// menyambungkan ke sql
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_errno) {
+    printf("Connect failed: %s\n", $conn->connect_error);
+    exit();
+}		
+?>
